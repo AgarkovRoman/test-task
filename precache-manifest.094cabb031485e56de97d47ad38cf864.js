da558c624a27d1c0387a1c67adceff730a0dc51e
Roman Agarkov <agarkovromachka@mail.ru>
2020-08-09 11:37:55 +0300

    Updates

---
+++ precache-manifest.094cabb031485e56de97d47ad38cf864.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86432781e5e5dbe9fdf6e2a4eb3f04f0",
+    "url": "/test-task/index.html"
+  },
+  {
+    "revision": "2ecca5417079f9907afd",
+    "url": "/test-task/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "72b6e76d2c3ef489772c",
+    "url": "/test-task/static/css/main.e65bac04.chunk.css"
+  },
+  {
+    "revision": "2ecca5417079f9907afd",
+    "url": "/test-task/static/js/2.4fdda147.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/test-task/static/js/2.4fdda147.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72b6e76d2c3ef489772c",
+    "url": "/test-task/static/js/main.64900443.chunk.js"
+  },
+  {
+    "revision": "6dc015072c6279bb1bac",
+    "url": "/test-task/static/js/runtime-main.e8037bfc.js"
+  }
+]);
